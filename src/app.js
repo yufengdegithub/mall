8b90adecd148bc0d3e92cc3844a8a29a7bbbe121
Yufeng <yangyuecho@126.com>
2018-06-28 17:30:34 +0800

    初始化文件

---
+++ src/app.js
@@ -0,0 +1,4 @@
+/**
+ * Created by ZD on 2018/6/28.
+ */
+console.log('hello webpack');
---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+/**
+ * Created by ZD on 2018/6/28.
+ */
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'app.js'
+    },
+    plugins: [new HtmlWebpackPlugin()]
+};
